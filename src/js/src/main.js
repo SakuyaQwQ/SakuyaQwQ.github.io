b6d1d84f937e10c28f92679d29e72b51a03955aa
SakuyaQwQ <46926438+SakuyaQwQ@users.noreply.github.com>
2021-04-08 22:44:14 +0800

    Add files via upload

---
+++ src/js/src/main.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    document.getElementById("goto_github").onclick = () => {
+        window.open("https://github.com/SakuyaQwQ");
+    };
+    document.getElementById("goto_leetcode").onclick = () => {
+        window.open("https://leetcode-cn.com/u/sakuyaqwq");
+    };
+    document.getElementById("goto_csdn").onclick = () => {
+        window.open("https://blog.csdn.net/weixin_43686836");
+    };
+};
